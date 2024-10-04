54b8f71fbc2894102bc043894bfc64391dbe0183
Іван <65630800+bahioiiia@users.noreply.github.com>
2024-10-05 01:34:05 +0300

    init

---
+++ src/basic/1.ts
@@ -0,0 +1,7 @@
+/* Перетворіть цей код на TypeScript, вказавши відповідні типи для всіх змінних. */
+
+const age = 50;
+const username = 'Max';
+const toggle = true;
+const empty = null;
+const callback = (a) => { return 100 + a };
